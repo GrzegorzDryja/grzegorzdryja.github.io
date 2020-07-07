3692ac59adc911f976f68e87823dfdec7d6c8fa7
GrzegorzDryja <GrzegorzDryja.via@gmail.com>
2020-07-07 16:06:55 +0200

    Full bio button
    Ugly and not sophisticated but works.  Need to make it better.

--- script.js
+++ script.js
@@ -2242,13 +2242,32 @@ System.register("file:///C:/Users/gd/GitHub/grzegorzdryja.github.io/src/mod", ["
         ],
         execute: function () {
             service_ts_2.getAboutMe().then((bio) => {
-                const md = document.createElement('about-me');
+                const markdown = document.createElement("about-me");
                 const main = document.getElementsByTagName("main");
-                md.innerHTML = rimu.render(bio);
-                main[0].appendChild(md);
+                const fullBio = rimu.render(bio);
+                markdown.innerHTML = fullBio.slice(0, 263) + "...";
+                main[0].appendChild(markdown);
+                const readFullBioButton = document.createElement("a");
+                readFullBioButton.innerHTML = "[ show full bio ]";
+                readFullBioButton.setAttribute("id", "fullBioButton");
+                markdown.appendChild(readFullBioButton);
+                document.getElementById("fullBioButton").addEventListener("click", () => {
+                    markdown.removeChild(readFullBioButton);
+                    markdown.innerHTML += fullBio.slice(264);
+                    readFullBioButton.innerHTML = "[ hide full bio ]";
+                    readFullBioButton.setAttribute("id", "hideBioButton");
+                    markdown.appendChild(readFullBioButton);
+                    document.getElementById("hideBioButton").addEventListener("click", () => {
+                        markdown.removeChild(readFullBioButton);
+                        markdown.innerHTML = fullBio.slice(0, 263);
+                        readFullBioButton.innerHTML = "[ show full bio ]";
+                        readFullBioButton.setAttribute("id", "fullBioButton");
+                        markdown.appendChild(readFullBioButton);
+                    });
+                });
             });
             mod_ts_1.default().then((repos) => {
-                const reposComponent = document.createElement('repos');
+                const reposComponent = document.createElement("repos");
                 const main = document.getElementsByTagName("main");
                 const p = document.createElement("p");
                 p.innerHTML = "Check out a few of my repos served by the github api:";
--- src/mod.ts
+++ src/mod.ts
@@ -5,14 +5,36 @@ import * as rimu from "https://deno.land/x/rimu/mod.ts";
 import checkDemoFiles from "../github/demo.ts";
 
 getAboutMe().then((bio) => {    
-    const md = document.createElement('about-me');
+    const markdown = document.createElement("about-me");
     const main = document.getElementsByTagName("main");
-    md.innerHTML = rimu.render(bio);
-    main[0].appendChild(md);
+    const fullBio = rimu.render(bio);   
+    markdown.innerHTML = fullBio.slice(0, 263) + "...";
+    main[0].appendChild(markdown);
+     
+    const readFullBioButton = document.createElement("a");
+          readFullBioButton.innerHTML ="[ show full bio ]";
+          readFullBioButton.setAttribute("id", "fullBioButton");
+    markdown.appendChild(readFullBioButton);
+
+    document.getElementById("fullBioButton").addEventListener("click", () => {  
+        markdown.removeChild(readFullBioButton);      
+        markdown.innerHTML += fullBio.slice(264);
+        readFullBioButton.innerHTML ="[ hide full bio ]";
+        readFullBioButton.setAttribute("id", "hideBioButton");
+        markdown.appendChild(readFullBioButton);
+        document.getElementById("hideBioButton").addEventListener("click", () => {  
+            markdown.removeChild(readFullBioButton);      
+            markdown.innerHTML = fullBio.slice(0, 263);
+            readFullBioButton.innerHTML ="[ show full bio ]";
+            readFullBioButton.setAttribute("id", "fullBioButton");
+            markdown.appendChild(readFullBioButton);
+        })
+    })
+
 });
 
 showRepos().then((repos) => {
-    const reposComponent = document.createElement('repos');
+    const reposComponent = document.createElement("repos");
     const main = document.getElementsByTagName("main");
     const p = document.createElement("p");
     p.innerHTML = "Check out a few of my repos served by the github api:";
