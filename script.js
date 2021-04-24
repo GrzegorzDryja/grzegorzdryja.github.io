1d2ca47254c2f6555099063686d1e687f44781f2
GrzegorzDryja <grzegorz.dryja@outlook.com>
2021-04-24 23:09:49 +0200

    Rimu dep, header class
    Somehow deno bundle dosn't handle rimu import.
    Header class mess around RWD.

--- script.js
+++ script.js
@@ -55,53 +55,57 @@ async function __default() {
     const arr = await getRepos();
     return arr;
 }
-function render1(source, opts = {
-}) {
-    updateOptions(opts);
-    return render(source);
-}
-init();
+// function render1(source, opts = {
+// }) {
+//     updateOptions(opts);
+//     return render(source);
+// }
+// init();
 getAboutMe().then((bio)=>{
-    const markdown = document.createElement("div");
-    const main = document.getElementsByTagName("aboutme");
-    const fullBio = render1(bio);
-    let status = "show";
-    markdown.innerHTML = fullBio.slice(0, 264) + "...";
-    main[0].appendChild(markdown);
-    const button = document.createElement("a");
-    button.innerHTML = status === "show" ? "[ show full bio ]" : "[ hide full bio ]";
-    button.setAttribute("id", status);
-    markdown.appendChild(button);
-    document.getElementById(status).addEventListener("click", ()=>{
-        if (status == "show") {
-            markdown.removeChild(button);
-            markdown.innerHTML += fullBio.slice(264);
-            status = "hide";
-            button.innerHTML = status === "show" ? "[ show full bio ]" : "[ hide full bio ]";
-            button.setAttribute("id", status);
-            markdown.appendChild(button);
-        } else {
-            markdown.innerHTML = fullBio.slice(0, 263) + "...";
-            status = "show";
-            button.innerHTML = status === "show" ? "[ show full bio ]" : "[ hide full bio ]";
-            button.setAttribute("id", status);
-            markdown.appendChild(button);
-        }
-    });
+    if (typeof document !== "undefined") {
+        const placeHolder = document.getElementsByTagName("about-me");
+        const fullBio = bio;
+        const bioDomElement = document.createElement("div");
+        let status = "show";
+        bioDomElement.innerHTML = fullBio.slice(0, 264) + "...";
+        placeHolder[0].appendChild(bioDomElement);
+        const button = document.createElement("a");
+        button.innerHTML = status === "show" ? "[ show full bio ]" : "[ hide full bio ]";
+        button.setAttribute("id", status);
+        bioDomElement.appendChild(button);
+        document.getElementById(status).addEventListener("click", ()=>{
+            if (status == "show") {
+                bioDomElement.removeChild(button);
+                bioDomElement.innerHTML += fullBio.slice(264);
+                status = "hide";
+                button.innerHTML = status === "show" ? "[ show full bio ]" : "[ hide full bio ]";
+                button.setAttribute("id", status);
+                bioDomElement.appendChild(button);
+            } else {
+                bioDomElement.innerHTML = fullBio.slice(0, 263) + "...";
+                status = "show";
+                button.innerHTML = status === "show" ? "[ show full bio ]" : "[ hide full bio ]";
+                button.setAttribute("id", status);
+                bioDomElement.appendChild(button);
+            }
+        });
+    }
 });
 __default().then((repos)=>{
-    const reposComponent = document.createElement("div");
-    const main = document.getElementsByTagName("repos");
-    const p = document.createElement("p");
-    p.innerHTML = "Check out a few of my repos served by the github api:";
-    const reposList = document.createElement("ul");
-    repos.forEach((repo)=>{
-        const li = document.createElement("li");
-        const language1 = repo.language || "in develop";
-        li.innerHTML = `<a href="${repo.htmlUrl}">${repo.name}</a><br />Programming language: <span>${language1}</span><br />Description: <i>${repo.description}</i>`;
-        reposList.appendChild(li);
-    });
-    reposComponent.appendChild(reposList);
-    reposComponent.insertBefore(p, reposList);
-    main[0].appendChild(reposComponent);
+    if (typeof document !== "undefined") {
+        const reposComponent = document.createElement("div");
+        const main = document.getElementsByTagName("repos");
+        const p = document.createElement("p");
+        p.innerHTML = "Check out a few of my repos served by the github api:";
+        const reposList = document.createElement("ul");
+        repos.forEach((repo)=>{
+            const li = document.createElement("li");
+            const language1 = repo.language || "in develop";
+            li.innerHTML = `<a href="${repo.htmlUrl}">${repo.name}</a><br />Programming language: <span>${language1}</span><br />Description: <i>${repo.description}</i>`;
+            reposList.appendChild(li);
+        });
+        reposComponent.appendChild(reposList);
+        reposComponent.insertBefore(p, reposList);
+        main[0].appendChild(reposComponent);
+    }
 });
--- src/mod.ts
+++ src/mod.ts
@@ -1,58 +1,60 @@
 import showRepos from "../github/mod.ts";
 import { getAboutMe } from "../github/service.ts";
-import * as rimu from "https://deno.land/x/rimu@11.1.14/mod.ts";
-import checkDemoFiles from "../github/demo.ts";
+import { render } from "https://deno.land/x/rimu@11.1.14/mod.ts";
+// import checkDemoFiles from "../github/demo.ts";
 import { GitHubRepo } from "../github/model.ts";
 
 getAboutMe().then((bio) => {
-    if(document){
-        const markdown = document.createElement("div");
-        const main = document.getElementsByTagName("aboutme");
-        const fullBio = rimu.render(bio);
-        let status = "show";   
-        markdown.innerHTML = fullBio.slice(0, 264) + "...";
-        main[0].appendChild(markdown);
-         
-        const button = document.createElement("a");
-              button.innerHTML = status === "show" ? "[ show full bio ]" : "[ hide full bio ]";
-              button.setAttribute("id", status);
-        markdown.appendChild(button);
-    
-        document.getElementById(status).addEventListener("click", () => {
-            if(status == "show") {
-                markdown.removeChild(button);      
-                markdown.innerHTML += fullBio.slice(264);
-                status = "hide";
-                button.innerHTML = status === "show" ? "[ show full bio ]" : "[ hide full bio ]";
-                button.setAttribute("id", status);
-                markdown.appendChild(button);
-            } else {
-                markdown.innerHTML = fullBio.slice(0, 263) + "...";
-                status = "show";
-                button.innerHTML = status === "show" ? "[ show full bio ]" : "[ hide full bio ]";
-                button.setAttribute("id", status);
-                markdown.appendChild(button) 
-            }
-        })
-    }    
-
+  if (typeof (document) !== "undefined") {
+    const placeHolder = document.getElementsByTagName("about-me");
+    const fullBio = render(bio);
+    const bioDomElement = document.createElement("div");
+    let status = "show";   
+    bioDomElement.innerHTML = fullBio.slice(0, 264) + "...";
+    placeHolder[0].appendChild(bioDomElement);
+      
+    const button = document.createElement("a");
+      button.innerHTML = status === "show" ? "[ show full bio ]" : "[ hide full bio ]";
+      button.setAttribute("id", status);
+    bioDomElement.appendChild(button);
+  
+    document.getElementById(status).addEventListener("click", () => {
+      if(status == "show") {
+        bioDomElement.removeChild(button);      
+        bioDomElement.innerHTML += fullBio.slice(264);
+        status = "hide";
+        button.innerHTML = status === "show" ? "[ show full bio ]" : "[ hide full bio ]";
+        button.setAttribute("id", status);
+        bioDomElement.appendChild(button);
+    } else {
+        bioDomElement.innerHTML = fullBio.slice(0, 263) + "...";
+        status = "show";
+        button.innerHTML = status === "show" ? "[ show full bio ]" : "[ hide full bio ]";
+        button.setAttribute("id", status);
+        bioDomElement.appendChild(button) 
+      }
+    })
+  }
 });
 
 showRepos().then((repos) => {
+  if (typeof (document) !== "undefined") {
     const reposComponent = document.createElement("div");
     const main = document.getElementsByTagName("repos");
     const p = document.createElement("p");
     p.innerHTML = "Check out a few of my repos served by the github api:";
     const reposList = document.createElement("ul");
-
+  
     repos.forEach((repo: GitHubRepo) => {               
-        const li = document.createElement("li");
-        // const demo = checkDemoFiles(repo.name);
-        const language = repo.language || "in develop"
-        li.innerHTML = `<a href="${repo.htmlUrl}">${repo.name}</a><br />Programming language: <span>${language}</span><br />Description: <i>${repo.description}</i>`;
-        reposList.appendChild(li);
+      const li = document.createElement("li");
+      // const demo = checkDemoFiles(repo.name);
+      const language = repo.language || "in develop"
+      li.innerHTML = `<a href="${repo.htmlUrl}">${repo.name}</a><br />Programming language: <span>${language}</span><br />Description: <i>${repo.description}</i>`;
+      reposList.appendChild(li);
     });
     reposComponent.appendChild(reposList);
     reposComponent.insertBefore(p, reposList);
     main[0].appendChild(reposComponent);
+ }
+
 })
